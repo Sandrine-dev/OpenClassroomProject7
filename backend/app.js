d73311f3bbd701002e0d9e2284ef3e4920e138a4
Sandrine-dev <sandrine.volpi@gmail.com>
2020-10-20 14:38:06 +0200

    modif package et app

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use(bodyParser.json());
 
 module.exports = app;
