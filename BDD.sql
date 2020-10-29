dcc1e1408b1aeb8a6892b0c73869da85631f9399
Sandrine-dev <sandrine.volpi@gmail.com>
2020-10-29 11:32:50 +0100

    modification plus ajout de liens

--- BDD.sql
+++ BDD.sql
@@ -1,60 +1,100 @@
-SET NAMES utf8;
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 29 oct. 2020 à 10:31
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
-DROP TABLE IF EXISTS Users;
-
-CREATE TABLE Users (
-    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nom VARCHAR(30) NOT NULL,
-    prenom VARCHAR(30) NOT NULL,
-    poste VARCHAR(30) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    mot_de_passe VARCHAR(100) NOT NULL,
-    photo_url VARCHAR(100) DEFAULT NULL,
-    photo_alt VARCHAR(30) DEFAULT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY (id),
-    UNIQUE KEY email (email)
-) ENGINE = INNODB;
-
-LOCK TABLES Users WRITE;
-
-UNLOCK TABLES;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS Post;
+--
+-- Base de données : `groupomania`
+--
 
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS Post (
-    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    users_id SMALLINT NOT NULL,
-    content text DEFAULT NULL,
-    gif_url VARCHAR(100) NULL,
-    gif_alt VARCHAR(30) DEFAULT NULL,
-    date_creation datetime NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY (id)
-    --CONSTRAINT user_post FOREIGN KEY (Users) REFERENCES Users(id)
-) ENGINE = INNOD;
+--
+-- Structure de la table `commentaires`
+--
 
-LOCK TABLES Post WRITE;
+DROP TABLE IF EXISTS `commentaires`;
+CREATE TABLE IF NOT EXISTS `commentaires` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `comment` text,
+  `date_creation` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `users_id` (`users_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `post`
+--
 
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `content` text,
+  `gif_url` varchar(100) DEFAULT NULL,
+  `gif_alt` varchar(30) DEFAULT NULL,
+  `date_creation` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `users_id` (`users_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS Commentaires;
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS Commentaires (
-    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    users_id SMALLINT NOT NULL,
-    comment text DEFAULT NULL,
-    date_creation datetime NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY (id)
-    --CONSTRAINT user_comment FOREIGN KEY (Users) REFERENCES Users(id)
-) ENGINE = INNODB;
+--
+-- Structure de la table `users`
+--
 
-LOCK TABLES Commentaires WRITE;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `poste` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `photo_url` varchar(100) DEFAULT NULL,
+  `photo_alt` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-UNLOCK TABLES;
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `User_comment` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `User_post` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
