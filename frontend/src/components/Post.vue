7c6427531ecbef9add75663152f538ef9ae2f489
Sandrine-dev <sandrine.volpi@gmail.com>
2021-01-27 10:32:06 +0100

    affichage des messages

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,46 +1,41 @@
 <template>
   <div class="messageSee">
-    <div class="allMessage">
-
-      <div v-for="message in message" class="message card" :key="message.id">
-        <div>
-          <h2>{{message.userId}}</h2>
-          <div class="content">
-            <p>{{ message.message }}</p>
-            {{ message.image_url }}
-          </div>
+    <div class="allMessage card">
+        <div class="user">
+          <h2>{{username}}</h2>
         </div>
+        <div class="content">
+            <p>{{message}}</p>
+            <img v-bind:src= "fileToDisplay" class="imagePost">
+        </div>
+     
 
         <div class="react-news"> 
         <!--<input type="text" class="comment" placeholder="Ecrire un commentaire..." @click="comment" />-->
         <!-- Like -->
-      </div>
+        </div>
 
-      </div>
-    
-
-      
     </div>
-
   </div>
 </template>
 
 <script>
-  import axios from "axios";
+  //import axios from "axios";
 
   export default {
     data () {
       return {
-        message: '',
+        username: '',
       }
     },
 
-  mounted () {
-    axios
-      .get ('http://localhost:3000/api/messages')
-      .then (response => this.message = response.data )
-      .catch ( error=> console.log(error))
-  }
+    props:{
+      message: String,
+      fileToDisplay: String,
+      userId: Number,
+      messageId:Number,
+    },
+
 }
 </script>
 
--- frontend/src/views/Wall.vue
+++ frontend/src/views/Wall.vue
@@ -2,14 +2,34 @@
   <div class="about">
 
     <section class="PostMessage">
-      <NewMessage></NewMessage>
+      <div class="container newMessage">
+    <div class="card">
+      <div class="card-body">
+        <form action="" class="panel panel-default">
+            <div class="d-flex align-items-start flex-column form-group">
+              <label for="text" class="panel-heading form-label">Nouveau message</label>
+              <textarea type="message" class="panel-body container-fluid form-contro" placeholder="nouveau message" rows="3" v-model="message" v-on:input="message = $event.target.value"></textarea>
+            </div>
+            <div class="mb-3 align-items-start">
+              <label for="formFileSm" class="form-label d-flex align-items-start">Ajoutez une image</label>
+              <input class="form-control form-control-sm" type="file" id="file" ref="fileInput" v-on:change="onFileUpload">
+            </div>
+            <div class="d-flex panel-footer form-group">
+              <button class="btn btn-primary w-25" @click="createMessage(message, image)" type="submit" value="Publier">Publier</button>
+
+
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
     </section>
 
     <section class="Actu">
       <h2 class="titre">Actualités de vos collègues</h2>
 
       <div class="container wallMessage">
-        <Post></Post>
+        <Post v-for="message in response" :key="message.id" :message="message.message" :fileToDisplay="message.image_url" :userId="message.userId" :messageId="message.id" class="allPost"></Post>
       </div>
     </section>
   </div>
@@ -18,14 +38,51 @@
 <script>
 
 import Post from '@/components/Post';
-import NewMessage from '@/components/NewMessage';
+import Axios from 'axios';
 
 
 export default {
+  components : {Post},
+  data () {
+    return {
+      selectedFile: null,
+      message: '',
+      fileToDisplay: '',
+      response: [],
+    }
+  },
 
-  components : {Post , NewMessage },
+  created (){
+    this.allPost();
+  },
+
+
+  methods :{
+    onFileUpload(event){
+      this.selectedFile = event.target.files[0];
+    },
+
+    createMessage(message) {
+
+      const fromData = new FormData()
+      fromData.append('image', this.selectedFile)
+      fromData.append('message', message)
+      Axios.post('http://localhost:3000/api/messages/new', fromData, {
+      }).then((res) => {
+        console.log(res);
+        this.allPost();
+      })
+    },
+
+    allPost: function() {
+      Axios
+      .get ('http://localhost:3000/api/messages')
+      .then ((response) => this.response = response.data )
+      .catch ( error=> console.log(error))
+    }
 
   }
+}
 
 </script>
 
